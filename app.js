99e13b2eddab0ecd1dbb18e5696d0cd5c525fa28
jeel-simform <jeel.v@simformsolutions.com>
2023-03-03 13:21:22 +0530

    all file structured and added functionality also

--- app.js
+++ app.js
@@ -1,61 +1,32 @@
 const fs=require('fs');
 
 const express=require('express')
+const morgan=require('morgan')
+
+const tourRouter=require('./routes/tourRoutes')
+const userRouter=require('./routes/userRoutes')
+
 const app=express();
 
 app.use(express.json());
+app.use(morgan('dev'))
 
-const port=process.env.PORT||3000;
+
 
 const tours= JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 
-app.get('/api/v1/tours',(req,res)=>{
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{
-            tours
-        }
-    })
-})
+app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/users',userRouter);
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-    // console.log(req.params);
-    const id =req.params.id * 1;
-    const tour=tours.find(el=>el.id ===id);
+// app.get('/api/v1/tours',getAllTours)
 
-    if(!tour){
-       return res.status(404).json({
-        status:'fail',
-        message:'Invalide ID'
-       })
-    }
-    res.json({
-        status:"success",
-        data:{
-            tours:tour
-        }
-    })
-})
+// app.get('/api/v1/tours/:id',getTour)
 
-app.post('/api/v1/tours',(req,res)=>{
-    // console.log(req.body);
-    const newId=tours[tours.length-1].id+1;
-    const newTour=Object.assign({id:newId},req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
-        res.status(201).json({
-            status:'success',
-            data:{
-                tour:newTour
-            }
-        })
+// app.post('/api/v1/tours',createTour)
 
-    })
-    // res.send('Done!')
-})
+// app.patch('/api/v1/tours/:id',updateTour)
 
-app.listen(port,()=>{
-    console.log('server is listen on port 3000');
-})
+// app.delete('/api/v1/tours/:id',deleteTour)
+
+module.exports=app;
---
+++ controller/userController.js
@@ -0,0 +1,36 @@
+
+exports.getAllUsers=(req,res)=>{
+    res.status(504).json({
+        status:'error',
+        message:'Not Implemented'
+    })
+}
+
+
+exports.createUser=(req,res)=>{
+    res.status(504).json({
+        status:'error',
+        message:'Not Implemented'
+    })
+}
+
+exports.getUser=(req,res)=>{
+    res.status(504).json({
+        status:'error',
+        message:'Not Implemented'
+    })
+}
+
+exports.updateUser=(req,res)=>{
+    res.status(504).json({
+        status:'error',
+        message:'Not Implemented'
+    })
+}
+
+exports.deleteUser=(req,res)=>{
+    res.status(504).json({
+        status:'error',
+        message:'Not Implemented'
+    })
+}
