ace22efdb3229accbb45aeecd7571442414a89bb
jeel-simform <jeel.v@simformsolutions.com>
2023-03-03 12:06:35 +0530

    added get post and id by get

---
+++ app.js
@@ -0,0 +1,61 @@
+const fs=require('fs');
+
+const express=require('express')
+const app=express();
+
+app.use(express.json());
+
+const port=process.env.PORT||3000;
+
+const tours= JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
+
+
+app.get('/api/v1/tours',(req,res)=>{
+    res.status(200).json({
+        status:'success',
+        results:tours.length,
+        data:{
+            tours
+        }
+    })
+})
+
+app.get('/api/v1/tours/:id',(req,res)=>{
+    // console.log(req.params);
+    const id =req.params.id * 1;
+    const tour=tours.find(el=>el.id ===id);
+
+    if(!tour){
+       return res.status(404).json({
+        status:'fail',
+        message:'Invalide ID'
+       })
+    }
+    res.json({
+        status:"success",
+        data:{
+            tours:tour
+        }
+    })
+})
+
+app.post('/api/v1/tours',(req,res)=>{
+    // console.log(req.body);
+    const newId=tours[tours.length-1].id+1;
+    const newTour=Object.assign({id:newId},req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
+        res.status(201).json({
+            status:'success',
+            data:{
+                tour:newTour
+            }
+        })
+
+    })
+    // res.send('Done!')
+})
+
+app.listen(port,()=>{
+    console.log('server is listen on port 3000');
+})
